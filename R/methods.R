76cc3aa84dc5a27cc753e174ae9158e7bcc95224
Eric Bryant <eeb2139@columbia.edu>
2015-06-04 13:11:37 -0400

    Add print method for dbcnf

---
+++ R/methods.R
@@ -0,0 +1,9 @@
+#-- cnf print method ----------------------------------------------------------
+#' @export
+
+print.dbcnf <-
+  function(x,
+           what = c('cnf', 'db', 'dir', 'name', 'table', 'update', 'keys')) {
+  cat(as.yaml(x[what]))
+}
+
